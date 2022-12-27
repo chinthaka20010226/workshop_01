560cbd5357b5eca80abc3a64734bbffa37a2b488
chinthaka20010226 <prabathchinthaka361@gmail.com>
2022-12-27 21:53:32 +0530

    modify first.py

--- first.py
+++ first.py
@@ -1,7 +1,40 @@
-def what_is_your():
-    x = input("what is your name? ")
-    return x
+print("well come to my computer quiz!")
 
-name = what_is_your()
-print(name)
-print("hi " + name)
+playing = input("Do you want to play? ")
+
+if playing.lower() != "yes":
+    quit()
+
+print("Okay, let's play game: ")
+score = 0
+
+answer = input("What does CPU stand for? ")
+if answer == "central processing unit":
+    print("correct!")
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What does GPU stand for? ")
+if answer == "graphics processing unit":
+    print("correct!")
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What does RAM stand for? ")
+if answer == "random access memory":
+    print("correct!")
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What does PSU stand for? ")
+if answer == "power supply unit":
+    print("correct!")
+    score += 1
+else:
+    print("incorrect!")
+
+print("You got" + str(score) + "questions crrect!")
+print("You got " + str((score/4) * 100) + "%.")
